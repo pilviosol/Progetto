d8d70eb0f9ea09bcbff1297576d7a0c4347c8ab0
Giulio Zanetti <hamalz@protonmail.com>
2019-12-15 23:35:23 +0100

    Aggiunto resizable canvas

--- test2.js
+++ test2.js
@@ -1,4 +1,3 @@
-
 var ctx = document.getElementById('colorWheel').getContext('2d');
 var chart = new Chart(ctx, {
   
@@ -37,9 +36,7 @@ var chart = new Chart(ctx, {
 }); 
 
 
-rgbCircle = [[255, 255, 0],[255, 132, 0],[255, 0, 0],[247, 0, 64],[239, 2, 126],[131, 1, 126],[19, 0, 123], [10, 82, 165],[0, 159, 197],[0, 147, 126],[0, 140, 57], [130, 198, 28], [255,255,255]]
-
-document.querySelector(".file-upload-image").style.display="none";
+rgbCircle = [[255, 255, 0],[255, 132, 0],[255, 0, 0],[247, 0, 64],[239, 2, 126],[131, 1, 126],[19, 0, 123], [10, 82, 165],[0, 159, 197],[0, 147, 126],[0, 140, 57], [130, 198, 28], [255,255,255]];
 
 function readURL(input) {
   if (input.files && input.files[0]) {
@@ -48,9 +45,7 @@ function readURL(input) {
     reader.onload = function(e) {
       background = document.getElementById('body-bg');
       $('.image-upload-wrap').hide();
-      $('.file-upload-image').attr('src', e.target.result);
       $('.file-upload-content').show();
-      $('body').removeClass('starting-bg')
       background.classList.remove('initial-bg');
       background.classList.add('updated-bg');
       background.style.backgroundImage = 'url(' + e.target.result + ')';
@@ -58,7 +53,19 @@ function readURL(input) {
       var context = canvas.getContext("2d");
       var img = new Image();
       img.onload = function() {
-        context.drawImage(img, 100,100)
+        if (img.width > img.height) {
+          canvas.width = 400;
+          var height = Math.round((img.height*400)/(img.width));
+          canvas.height = height;
+          context.drawImage(img,0,0,400,height)
+        }
+        else {
+          canvas.height = 400;
+          var width = Math.round((img.width*400)/(img.height));
+          canvas.width = width;
+          context.drawImage(img,0,0,width,400)
+        }
+        
       }
       img.src = e.target.result
     };
@@ -69,7 +76,6 @@ function readURL(input) {
   
   else {
     removeUpload();
- 
   }
   
 }
@@ -85,7 +91,3 @@ $('.image-upload-wrap').bind('dragover', function () {
 	$('.image-upload-wrap').bind('dragleave', function () {
 		$('.image-upload-wrap').removeClass('image-dropping');
 });
-
-
-
-  
