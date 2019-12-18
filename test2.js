c64127259468c33daa33ad0430f3afd359af637d
Giulio Zanetti <hamalz@protonmail.com>
2019-12-18 23:25:48 +0100

    Aggiunta palette con sfumature

--- test2.js
+++ test2.js
@@ -37,6 +37,24 @@ var chart = new Chart(ctx, {
 
 
 rgbCircle = [[255, 255, 0], [255, 132, 0], [255, 0, 0], [247, 0, 64], [239, 2, 126], [131, 1, 126], [19, 0, 123], [10, 82, 165], [0, 159, 197], [0, 147, 126], [0, 140, 57], [130, 198, 28], [0, 0, 0], [23, 23, 23], [46, 46, 46], [69, 69, 69], [92, 92, 92], [115, 115, 115], [138, 138, 138], [161, 161, 161], [184, 184, 184], [207, 207, 207], [230, 230, 230], [255, 255, 255]];
+hslCircle = [[255, 0, 0], [255, 127, 0], [255, 255, 0], [127, 255, 0], [0, 255, 0], [0, 255, 127], [0, 255, 255], [0, 127, 255], [0, 0, 255], [127, 0, 255], [255, 0, 255], [255, 0, 127]];
+var palette = [];
+
+for(i=0; i<12; i++) {
+  var currentColor = hslCircle[i];
+  var hue = rgbToHsl(currentColor[0], currentColor[1], currentColor[2])[0];
+  for(j=1; j<6; j++) { // saturation values
+    for(k=1; k<5; k++) { // luminance values
+      palette.push(hslToRgb(hue, (2*j)/10, (2*k)/10));
+      console.log([hue, (2*j)/10, (2*k)/10]);
+      console.log(hslToRgb(hue, (2*j)/10, (2*k)/10));
+    }
+  }
+}
+for(h=0; h<6; h++) { // greys
+  palette.push([51*h, 51*h, 51*h]);
+}
+console.log(palette);
 
 var imageData;
 var width;
@@ -51,7 +69,7 @@ var opts = {
 	dithKern: null,          // dithering kernel name, see available kernels in docs below
 	dithDelta: 0,            // dithering threshhold (0-1) e.g: 0.05 will not dither colors with <= 5% difference
 	dithSerp: false,         // enable serpentine pattern dithering
-	palette: rgbCircle,      // a predefined palette to start with in r,g,b tuple format: [[r,g,b],[r,g,b]...]
+	palette: palette,      // a predefined palette to start with in r,g,b tuple format: [[r,g,b],[r,g,b]...]
 	reIndex: true,          // affects predefined palettes only. if true, allows compacting of sparsed palette once target palette size is reached. also enables palette sorting.
 	useCache: false,          // enables caching for perf usually, but can reduce perf in some cases, like pre-def palettes
 	cacheFreq: 10,           // min color occurance count needed to qualify for caching
@@ -125,3 +143,67 @@ $('.image-upload-wrap').bind('dragover', function () {
 	$('.image-upload-wrap').bind('dragleave', function () {
 		$('.image-upload-wrap').removeClass('image-dropping');
 });
+
+
+/*
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ */
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, l ];
+}
+
+/*
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ */
+
+function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [ r * 255, g * 255, b * 255 ];
+}
